f02140a846618c093252f58deabc4a4118f7e7df
Caio Silva <124459199+CaioSilva14@users.noreply.github.com>
2023-08-17 15:50:32 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function abrirMenu(){
+    document.getElementById("menu-oculto").style.width="250px";
+    document.getElementById("principal").style.marginLeft="250px";
+}
+function fecharMenu(){
+    document.getElementById("menu-oculto").style.width="0";
+    document.getElementById("menu-oculto").style.MarginLeft="0";
+}
